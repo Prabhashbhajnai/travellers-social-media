80fb7dca3739cfa0461e088cdae292a21bbc35dc
Prabhashbhajani <prabhashbhajani@gmail.com>
2023-05-30 14:44:52 +0530

    Post routes added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,47 @@
-export const getPosts = (req, res) => {
-    res.send("This Works!")
+// models
+import PostMessage from "../models/postMessage.js"
+
+/*
+Route     /
+Des       Get all posts
+Params    none
+BODY      none
+Access    Private
+Method    GET  
+*/
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessage = await PostMessage.find()
+
+        console.log(postMessage)
+
+        res.status(200).json(postMessage)
+    } catch (error) {
+        res.status(404).json({
+            message: error.message
+        })
+    }
+}
+
+/*
+Route     /
+Des       Create Post
+Params    none
+BODY      post
+Access    Private
+Method    POST  
+*/
+export const createPosts = async (req, res) => {
+    const post = req.body
+
+    const newPost = new PostMessage(post)
+
+    try {
+        await newPost.save()
+
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json({ message: error.message })
+    }
 }
