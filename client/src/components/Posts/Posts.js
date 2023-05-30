3b46f7d59638c39a5f8067d61a77759cb63dab2a
Prabhashbhajani <prabhashbhajani@gmail.com>
2023-05-30 23:39:50 +0530

    form, post and posts styles added

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -3,7 +3,12 @@ import React from 'react'
 // components
 import Post from './Post/Post'
 
+// styles
+import useStyles from './styles' 
+
 const Posts = () => {
+    const classes = useStyles()
+
     return (
         <>
             <h1>Posts</h1>
