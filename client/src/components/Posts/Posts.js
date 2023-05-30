567389a56af017c6d24aa33f3b7426770900f2c2
Prabhashbhajani <prabhashbhajani@gmail.com>
2023-05-30 23:14:55 +0530

    homepage styles added

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// components
+import Post from './Post/Post'
+
+const Posts = () => {
+    return (
+        <>
+            <h1>Posts</h1>
+            <Post />
+            <Post />
+        </>
+    )
+}
+
+export default Posts
